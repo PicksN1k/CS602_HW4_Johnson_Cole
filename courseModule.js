b37f3fef3864f38dfaa525cd996f8c0bc0c1045b
PicksN1k <csjohnson2838@gmail.com>
2026-07-27 18:05:37 -0400

    modified:   courseModule.js

--- courseModule.js
+++ courseModule.js
@@ -21,7 +21,7 @@ export const lookupByCourseId =  async (id) => {
 export const lookupByCourseName = async (name) => {
   const courses = await Course.find({}).populate("coordinator");
 
-  let pattern = new RegExp(name, "i");
+  const pattern = new RegExp(name.trim(), "i");
 
   const result = courses.filter(course =>
     pattern.test(course.courseName)
