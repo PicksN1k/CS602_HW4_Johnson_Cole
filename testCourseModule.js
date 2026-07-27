13d9b081728e728412327115bcaa6eb2c17f19ce
PicksN1k <csjohnson2838@gmail.com>
2026-07-27 16:49:06 -0400

    modified:   testCourseModule.js

--- testCourseModule.js
+++ testCourseModule.js
@@ -23,6 +23,19 @@ result = await courseDB.getCourseDescription('632');
 console.log(result);
 
 // Include other test cases
+console.log(JSON.stringify(
+    await courseDB.lookupByCourseId("^CS 6"), null, 2));
 
+console.log(JSON.stringify(
+    await courseDB.lookupByCourseName("Data"), null, 2));
+
+console.log(JSON.stringify(
+    await courseDB.lookupByCoordinator("epinsky"), null, 2));
+
+console.log(JSON.stringify(
+    await courseDB.getRandomCourse(), null, 2));
+
+console.log(JSON.stringify(
+    await courseDB.getCourseDescription("602"), null, 2));
 
 await courseDB.connection.disconnect();
